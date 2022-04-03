ac8c351742f3eadcca238f2598bb41c280022a0f
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-04-03 18:05:17 +0530

    API Demo Application @ 2022 #032AbhishekChoksi. All rights reserved.

---
+++ app/src/main/java/com/example/apidemo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.apidemo;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    String API_URL = "https://jsonplaceholder.typicode.com/posts";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, API_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        textView.setText(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText(error.getMessage());
+                    }
+                });
+        requestQueue.add(stringRequest);
+    }
+}
---
+++ app/src/main/java/com/example/apidemo/MainActivity2.java
@@ -0,0 +1,64 @@
+package com.example.apidemo;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
+
+    String API_URL = "https://jsonplaceholder.typicode.com/todos";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        textView = findViewById(R.id.textViewMainActivity2);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, API_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        textView.setText(response);
+                        //Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText(error.getMessage());
+                       // Toast.makeText(getApplicationContext(), "Error while calling api", Toast.LENGTH_LONG).show();
+                    }
+                }
+        ){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String,String>();
+                params.put("userId","1");
+                params.put("title","new title");
+                params.put("completed","false");
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+    }
+}
